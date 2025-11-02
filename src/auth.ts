9dc49d3a8100dac5c4f67b8185d3ad4fdd362a98
Nicklas Frahm <nicklas.frahm@gmail.com>
2025-11-02 22:11:54 +0100

    feat(app): configure container auth and remove filesystem client

--- src/auth.ts
+++ src/auth.ts
@@ -3,12 +3,10 @@ import GitHub from "next-auth/providers/github";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [GitHub],
+  trustHost: true,
   callbacks: {
     jwt({ token, user }) {
-      // User is available during sign-in.
-      if (user) {
-        console.log("User signed in:", user);
-      }
+      // TODO: Extend the JWT to include the accessToken.
 
       return token;
     },
